06a670a2a047f31c637100ebc4eeb58dbd95b1fc
yuxuan zhang <yuxuan@celer.network>
2021-11-16 16:10:05 +0800

    init

--- contracts/IncentiveEventReward.sol
+++ contracts/IncentiveEventReward.sol
@@ -5,8 +5,9 @@ pragma solidity 0.8.9;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
-contract StakingReward {
+contract StakingReward is Ownable {
     using SafeERC20 for IERC20;
 
     IERC20 public immutable CELER_TOKEN;
