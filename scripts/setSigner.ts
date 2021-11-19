88c92190cc57ea5ac19e73f439efb95c549cb6d7
yuxuan zhang <yuxuan@celer.network>
2021-11-19 15:43:25 +0800

    init

--- scripts/setSigner.ts
+++ scripts/setSigner.ts
@@ -4,7 +4,7 @@ import {ethers} from "hardhat";
 async function main() {
     const contractFactory = await ethers.getContractFactory("IncentiveEventsReward");
     const contract = await contractFactory.attach("0x80619B477D2b4Cb9204E7B161dC3Aa09BA732113");
-    await contract.setSigner("0x9F9F4828f32e3797141ae2232d4f06bFF1915bCa");
+    await contract.setSigner("0x427ff189e06a6bd746717ca208c5b193e7496313");
 }
 
 main().catch((error) => {
