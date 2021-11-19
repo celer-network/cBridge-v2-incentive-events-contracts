7818aff64be4cc40f799b5356b75de1dd09c4552
Michael Zhou <zhoumotongxue008@gmail.com>
2021-11-19 11:26:11 +0800

    Add solt script for manual verification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -118,7 +118,7 @@ const config: HardhatUserConfig = {
     disambiguatePaths: false
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS === 'true' ? true : false,
+    enabled: process.env.REPORT_GAS === 'true',
     noColors: true,
     outputFile: 'reports/gas_usage/summary.txt'
   },
